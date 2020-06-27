6485e7296c6fcf89cfdef29368e964fdf1a276a6
Ryan <devogelryan@gmail.com>
2020-06-27 18:00:58 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+inGame = True
+
+while inGame:
+	number = input("Type a number for a upper bound: ")
+
+	if number.isdigit():
+		print("Let the game begin")
+		number = int(number)
+		inGame = False
+
+	else:
+		print("Invalid input! Try Again!")
+
+secret = random.randint(1, number)
+guess = None
+count = 1
+
+while guess != secret:
+	guess = input("Pls type a number between 1 and " + str(number) + ": ")
+	if guess.isdigit():
+		guess = int(guess)
+
+	if guess == secret:
+		print("You got it!")
+	else:
+		print("Wrong! Try again!")
+		count += 1
+
+print("It took you " + str(count) + " Times.")
